b979c8ec8637e8cc24d1f90069943895e1907249
Kevin Miller <ksmill327@gmail.com>
2019-08-09 17:33:59 -0700

    Other changes, found problem in calc_orig_new function... need to fix

--- util.py
+++ util.py
@@ -15,7 +15,39 @@ from sklearn.datasets import make_moons
 import copy
 
 
+################# Graph generation and other Calculations
 
+def sqdist(X, Y):
+    # Michael Luo code  - X is (d x m) np array, Y is (d x n) np array
+    # returns D_ij = |x_i - y_j|^2 matrix
+    # better than KD tree for larger dimensions?
+    m = X.shape[1]
+    n = Y.shape[1]
+    Yt = Y.T
+    XX = np.sum(X*X, axis=0)
+    YY = np.sum(Yt*Yt, axis=1).reshape(n,1)
+    return np.tile(XX, (n,1)) + np.tile(YY, (1,m)) - 2*Yt.dot(X)
+
+# Making a similarity graph
+def make_sim_graph(X, k_nn=5):
+    N = X.shape[0]
+    # Make weighted similarity graph, in W
+    D = sqdist(X.T,X.T)
+    ind_knn = np.argsort(D, axis=1)[:,1:k_nn+1]
+    Dknn = D[(np.arange(N).reshape(N,1),ind_knn)]
+
+    I = np.tile(np.arange(N).reshape(N,1), (1,k_nn)).flatten()
+    J = ind_knn.flatten()
+    Dmean = (np.sum(Dknn, axis=1)/k_nn).reshape(N,1)
+    w_sp = np.divide(Dknn, Dmean)
+    w_sp = np.exp(-(w_sp * w_sp))
+    W = sps.csr_matrix((w_sp.flatten() , (I, J)), shape=(N,N))
+    W = 0.5*(W+W.T)
+
+    return W
+
+
+# Gaussian clusters data
 def generate_data_graphs(Ns, means, Covs, k_nn=5):
     '''
     inputs   :
@@ -35,42 +67,17 @@ def generate_data_graphs(Ns, means, Covs, k_nn=5):
 
 
     # Make weighted similarity graph, in W
-    D = sqdist(X.T,X.T)
-    ind_knn = np.argsort(D, axis=1)[:,1:k_nn+1]
-    Dknn = D[(np.arange(N).reshape(N,1),ind_knn)]
-
-    I = np.tile(np.arange(N).reshape(N,1), (1,k_nn)).flatten()
-    J = ind_knn.flatten()
-    Dmean = (np.sum(Dknn, axis=1)/k_nn).reshape(N,1)
-    w_sp = np.divide(Dknn, Dmean)
-    w_sp = np.exp(-(w_sp * w_sp))
-    W = sps.csr_matrix((w_sp.flatten() , (I, J)), shape=(N,N))
-    W = 0.5*(W+W.T)
-
+    W = make_sim_graph(X, k_nn)
     return X, W
-
-def sqdist(X, Y):
-    # Michael Luo code  - X is (d x m) np array, Y is (d x n) np array
-    # returns D_ij = |x_i - y_j|^2 matrix
-    # better than KD tree for larger dimensions?
-    m = X.shape[1]
-    n = Y.shape[1]
-    Yt = Y.T
-    XX = np.sum(X*X, axis=0)
-    YY = np.sum(Yt*Yt, axis=1).reshape(n,1)
-    return np.tile(XX, (n,1)) + np.tile(YY, (1,m)) - 2*Yt.dot(X)
-
-
-
-# Main setup, defining the node set, fidelity nodes, ground truth u and observations y, and corresponding
-# B and H matrices
 
 
 def get_eig_Lnorm(W):
     L_sym = csgraph.laplacian(W, normed=True)
     [w, v] = sp.linalg.eigh(L_sym.toarray())
-
     return w, v
+
+
+
 
 
 ########################## Single Updates #######################
@@ -94,10 +101,11 @@ def calc_next_C_and_m(m, C, y, lab, k, y_k, gamma2):
 
     # calculate C_k -- the posterior of adding k, y_k
     C_k = C - (1./(gamma2 + ckk))*np.outer(ck,ck)
-
     return m_k, C_k
 
 
+
+# Transform the vector m into probabilities, while still respecting the threshold value currently at 0
 def get_probs(m):
     m_probs = m.flatten() # simple fix to get probabilities that respect the decision bdry
     m_probs[np.where(m_probs >0)] /= 2.*np.max(m_probs)
@@ -148,28 +156,7 @@ def plot_iter(stats, X, k_next=-1):
 
 
 
-
-
-    ########################### 2 Moons Functions
-
-def make_sim_graph(X, k_nn=5):
-    N = X.shape[0]
-    # Make weighted similarity graph, in W
-    D = sqdist(X.T,X.T)
-    ind_knn = np.argsort(D, axis=1)[:,1:k_nn+1]
-    Dknn = D[(np.arange(N).reshape(N,1),ind_knn)]
-
-    I = np.tile(np.arange(N).reshape(N,1), (1,k_nn)).flatten()
-    J = ind_knn.flatten()
-    Dmean = (np.sum(Dknn, axis=1)/k_nn).reshape(N,1)
-    w_sp = np.divide(Dknn, Dmean)
-    w_sp = np.exp(-(w_sp * w_sp))
-    W = sps.csr_matrix((w_sp.flatten() , (I, J)), shape=(N,N))
-    W = 0.5*(W+W.T)
-
-    return W
-
-def calc_orig2(v, w, B, fid, tau, alpha, gamma2):
+def calc_orig(v, w, B, fid, tau, alpha, gamma2):
     N = v.shape[0]
     y = np.zeros(N)  # this will already be in the expanded size, as if (H^Ty)
     y[fid[1]] = 1.
@@ -188,12 +175,13 @@ def calc_orig2(v, w, B, fid, tau, alpha, gamma2):
 
     return m, C, y
 
-def calc_orig2_new(v, w, B, fid, labeled, unlabeled, tau, alpha, gamma2):
+def calc_orig_new(v, w, B, fid, labeled, unlabeled, tau, alpha, gamma2):
     N = v.shape[0]
     y = np.zeros(N)  # this will already be in the expanded size, as if (H^Ty)
     y[fid[1]] = 1.
     y[fid[-1]] = -1.
 
+    N_prime = len(labeled)
     w_inv = (tau ** (2 * alpha)) * np.power(w + tau**2., -alpha)     # diagonalization of C_t,e
     C_tau = v.dot((v*w_inv).T)
     C_ll = C_tau[np.ix_(labeled, labeled)]
@@ -202,12 +190,12 @@ def calc_orig2_new(v, w, B, fid, labeled, unlabeled, tau, alpha, gamma2):
     A_inv = sla.inv(C_ll)
     Block1 = C_all_l.dot(A_inv)
     C = C_tau - Block1.dot(C_all_l.T)
-    m = Block1.dot(y[labeled])
+    m = Block1.dot(y[labeled]).flatten()
 
     return m, C, y
 
 
-def run_next2(m, C, y, lab, unlab, fid, ground_truth, gamma2):
+def run_next(m, C, y, lab, unlab, fid, ground_truth, gamma2):
     tic = time.clock()
     risks = []
     m_probs = get_probs(m)
@@ -234,13 +222,15 @@ def run_next2(m, C, y, lab, unlab, fid, ground_truth, gamma2):
 
 
 
-def calc_stats2(m, fid, gt_flipped, _print=False):
+def calc_stats(m, fid, gt_flipped, _print=False):
     stats = {}
-    if m.shape[1] == 1:
-        m = m.reshape(1,m.size)
+
+    m = m.reshape(1,m.size)
     N = m.shape[1]
     m1 = np.where(m >= 0)[1]
     m2 = np.where(m < 0)[1]
+
+
     sup1 = fid[1]
     sup2 = fid[-1]
     corr1 = list(set(m1).intersection(set(gt_flipped[1])))
@@ -298,11 +288,12 @@ def run_test_AL(X, v, w, fid, ground_truth, tag2=(0.01, 1.0, 0.00001), test_opts
     B_diag = np.zeros(N)
     B_diag[labeled] = 1.
     B = sp.sparse.diags(B_diag, format='lil')
-    m, C, y = calc_orig2(v, w, B, fid, tau, alpha, gamma2)
+    m, C, y = calc_orig(v, w, B, fid, tau, alpha, gamma2)
+    #m, C, y = calc_orig_new(v, w, B, fid, labeled, unlabeled, tau, alpha, gamma2) # error occurring with this?
 
     # Calculate the error of the classification resulting from this initial solution
     ERRS = []
-    error, stats_obj = calc_stats2(m, fid, gt_flipped)
+    error, stats_obj = calc_stats(m, fid, gt_flipped)
     ERRS.append((-1,error))
     if verbose:
         print('Iter = 0')
@@ -314,8 +305,8 @@ def run_test_AL(X, v, w, fid, ground_truth, tag2=(0.01, 1.0, 0.00001), test_opts
 
     # AL choices
     for l in range(iters):
-        k, m, C, y, labeled, unlabeled, fid = run_next2(m, C, y, labeled, unlabeled, fid, ground_truth, gamma2)
-        error, stats_obj = calc_stats2(m, fid, gt_flipped)
+        k, m, C, y, labeled, unlabeled, fid = run_next(m, C, y, labeled, unlabeled, fid, ground_truth, gamma2)
+        error, stats_obj = calc_stats(m, fid, gt_flipped)
         ERRS.append((k,error))
         M[l] = m
         if verbose:
@@ -355,11 +346,12 @@ def run_test_rand(X, v, w, fid, ground_truth, tag2=(0.01, 1.0, 0.00001), test_op
     B_diag = np.zeros(N)
     B_diag[labeled] = 1.
     B = sp.sparse.diags(B_diag, format='lil')
-    m, C, y = calc_orig2(v, w, B, fid, tau, alpha, gamma2)
+    m, C, y = calc_orig(v, w, B, fid, tau, alpha, gamma2)
+    #m, C, y = calc_orig_new(v, w, B, fid, labeled, unlabeled, tau, alpha, gamma2) # error occurring with this?
 
     # Calculate the error of the classification resulting from this initial solution
     ERRS_rand = []
-    error, stats_obj = calc_stats2(m, fid, gt_flipped)
+    error, stats_obj = calc_stats(m, fid, gt_flipped)
     ERRS_rand.append((-1,error))
     if verbose:
         print('Iter = 0')
@@ -381,7 +373,7 @@ def run_test_rand(X, v, w, fid, ground_truth, tag2=(0.01, 1.0, 0.00001), test_op
         unlabeled.remove(k_next)
 
         M_rand[l] = m
-        error, stats_obj = calc_stats2(m, fid, gt_flipped)
+        error, stats_obj = calc_stats(m, fid, gt_flipped)
         ERRS_rand.append((k_next, error))
         if verbose:
             print('Iter = %d' % (l + 1))
@@ -420,12 +412,37 @@ def run_next2_KL(m, C, y, lab, unlab, fid, ground_truth, gamma2, W):
     return k_next, m_next, C_next, y, lab, unlab, fid
 
 
-def run_test_AL2_KL(X, v, w, W, labeled, unlabeled, fid, ground_truth, gt_flipped, tau, alpha, gamma2, iters=5, verbose=False):
+def run_test_AL2_KL(X, v, w, fid, ground_truth, tag2=(0.01, 1.0, 0.00001), test_opts=(10, False)):
+    '''
+    Inputs:
+        X : (N x d) data matrix with the data points as columns
+        v : (N x N) eigenvectors (as columns)
+        w : (N, ) eigenvalues numpy array
+        fid : dictionary with fidelity indices (class_i, [i_1, i_2, ...])
+        tag2 : tuple (tau, alpha, gamma2). Default tau = 0.01, alpha = 1.0, gamma2 = 0.00001
+        test_opts : tuple (iters, verbose). Default (10, False)
+                (iters int, verbose bool)
+    '''
     N = len(ground_truth)
+    tau, alpha, gamma2 = tag2
+    iters, verbose = test_opts
+
+    # Prepare datastructures for labeling uses later
+    gt_flipped = {}
+    indices = np.array(list(range(N)))
+    labeled = set()
+    for k in fid.keys():
+        k_mask = indices[ground_truth ==k]
+        gt_flipped[k] = k_mask
+        labeled = labeled.union(set(fid[k]))
+    unlabeled = sorted(list(set(indices) - labeled))
+    labeled = sorted(list(labeled))
+
+
+    # Initial solution - find m and C, keep track of y
     B_diag = np.zeros(N)
     B_diag[labeled] = 1.
     B = sp.sparse.diags(B_diag, format='lil')
-
     m, C, y = calc_orig2(v, w, B, fid, tau, alpha, gamma2)
 
     ERRS = []
